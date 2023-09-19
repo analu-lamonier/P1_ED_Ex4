1e41e5253cebb5cd59ce5ac65cb13b5ae22c7347
Ana Luiza Lamonier <88122547+analu-lamonier@users.noreply.github.com>
2023-09-19 17:19:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caProvaEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Soma(int[] vetor, int t)
+            {
+                if (t == 0)
+                    return 0;
+                else
+                    return vetor[t - 1] + Soma(vetor, t - 1);
+            }
+
+            int[] vet = new int[] { 1, 2, 3, 4, 5, 6 };
+
+
+            int res = Soma(vet, 6);
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
